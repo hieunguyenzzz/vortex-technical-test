1df845ffc39f546e82593baf0e6ff7d5d75e4db3
Hieu Nguyen <hieu@junowebdesign.com>
2017-07-20 13:55:00 +0700

    3. Change JS & CSS Merging

---
+++ htdocs/app/code/local/Vortex/Resourcemerging/Helper/Data.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Author: Hieu Nguyen
+ */ 
+class Vortex_Resourcemerging_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ htdocs/app/code/local/Vortex/Resourcemerging/Model/Design/Package.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * @author: Hieu Nguyen
+ */
+class Vortex_Resourcemerging_Model_Design_Package extends Mage_Core_Model_Design_Package
+{
+    /**
+     * Merge specified javascript files and return URL to the merged file on success
+     *
+     * @param $files
+     * @return string
+     */
+    public function getMergedJsUrl($files)
+    {
+        $targetFilename = $this->_getTargetFilename($files) . '.js';
+        $targetDir = $this->_initMergerDir('js');
+        if (!$targetDir) {
+            return '';
+        }
+        if ($this->_mergeFiles($files, $targetDir . DS . $targetFilename, false, null, 'js')) {
+            return Mage::getBaseUrl('media', Mage::app()->getRequest()->isSecure()) . 'js/' . $targetFilename;
+        }
+        return '';
+    }
+
+    /**
+     * Merge specified css files and return URL to the merged file on success
+     *
+     * @param $files
+     * @return string
+     */
+    public function getMergedCssUrl($files)
+    {
+        // secure or unsecure
+        $isSecure = Mage::app()->getRequest()->isSecure();
+        $mergerDir = $isSecure ? 'css_secure' : 'css';
+        $targetDir = $this->_initMergerDir($mergerDir);
+        if (!$targetDir) {
+            return '';
+        }
+
+        // base hostname & port
+        $baseMediaUrl = Mage::getBaseUrl('media', $isSecure);
+        $hostname = parse_url($baseMediaUrl, PHP_URL_HOST);
+        $port = parse_url($baseMediaUrl, PHP_URL_PORT);
+        if (false === $port) {
+            $port = $isSecure ? 443 : 80;
+        }
+
+        // merge into target file
+        $targetFilename = $this->_getTargetFilename($files, "|{$hostname}|{$port}") . '.css';
+        $mergeFilesResult = $this->_mergeFiles(
+            $files, $targetDir . DS . $targetFilename,
+            false,
+            array($this, 'beforeMergeCss'),
+            'css'
+        );
+        if ($mergeFilesResult) {
+            return $baseMediaUrl . $mergerDir . '/' . $targetFilename;
+        }
+        return '';
+    }
+
+    /**
+     * get target file name for merging base on hash of supplied files content
+     *
+     * @param $files
+     * @param string $append
+     * @return string
+     */
+    protected function _getTargetFilename($files, $append = '')
+    {
+        $result = array();
+        foreach ($files as $file) {
+            $result[] = hash_file('md5', $file);
+        }
+
+        return md5(implode(',', $result) . $append);
+    }
+}
